d0ff7abb7f5818e631b3551621747f328ee3dae6
Ernesto Pérez Nieto <ernesto.pereznieto@abcam.com>
2016-10-19 14:07:48 +0100

    Added COrs to the java service, still not working need to debug

--- src/main/java/nestosoft/calculator/controllers/CalculatorController.java
+++ src/main/java/nestosoft/calculator/controllers/CalculatorController.java
@@ -3,11 +3,15 @@ package nestosoft.calculator.controllers;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import nestosoft.calculator.models.Result;
 
+
 @RestController
 public class CalculatorController {
+        
+    @CrossOrigin
     @RequestMapping("/add/{num1}/{num2}")
     public Result add( @PathVariable Integer num1,  @PathVariable Integer num2) {
         return new Result(num1, num2);
