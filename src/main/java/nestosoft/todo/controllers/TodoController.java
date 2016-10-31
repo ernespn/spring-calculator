5634f8651e20b88af8982aa7f12f9f569ed43314
Ernesto Perez <ernespn@gmail.com>
2016-10-31 19:59:47 +0000

    add cross origin to the todo service

--- src/main/java/nestosoft/todo/controllers/TodoController.java
+++ src/main/java/nestosoft/todo/controllers/TodoController.java
@@ -3,6 +3,7 @@ package nestosoft.todo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,23 +19,27 @@ public class TodoController {
 	@Autowired
 	private TodoService todoService;
 	
+	@CrossOrigin
 	@RequestMapping(value="/todo", method=RequestMethod.GET)
 	public List<Todo> GetAllTodos(){
 		return this.todoService.GetAllTodos();
 	}
 	
+	@CrossOrigin
 	@RequestMapping(value="/todo/{id}", method=RequestMethod.GET)
 	public Todo GetById(@PathVariable String id){
 		return this.todoService.GetById(id);
 	}
 	
+	@CrossOrigin
 	@RequestMapping(value="/todo", method=RequestMethod.POST)
 	public void SaveTodo(@RequestBody Todo todo){
 		this.todoService.SaveTodo(todo);
 	}
 	
+	@CrossOrigin
 	@RequestMapping(value="/todo/{id}", method=RequestMethod.DELETE)
-	public void SaveTodo(@PathVariable String id){
+	public void DeleteTodo(@PathVariable String id){
 		this.todoService.DeleteTodo(id);
 	}
 }
